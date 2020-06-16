6a99b2c03abc58597fce41c57c90cf266694c723
Henrik Håkansson <henrikhakansson95@gmail.com>
2020-06-16 19:15:42 +0200

    Raise error if not success status and change wordpress username

--- app/request_handler.py
+++ app/request_handler.py
@@ -14,8 +14,11 @@ def api_request(method, api_endpoint, error_message, error_category, query_param
             r = requests.post(url=api_endpoint, params=query_params, headers=headers)
         else:
             raise Exception(config['Errors']['request_bug'])
+        if r.status_code != 200:
+            raise HTTPError()
     except HTTPError:
         raise Exception(error_message, error_category)
+
     r.encoding = 'utf-8'
     return r.text
 
