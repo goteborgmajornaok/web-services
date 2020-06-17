54a4235696cf87bb6f5bd75da8341eb8c2f84279
Henrik Håkansson <henrikhakansson95@gmail.com>
2020-06-17 16:40:00 +0200

    Update cron time for user inventory

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ from app.user_inventory import user_inventory
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', seconds=30)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=17)
 def run_user_inventory():
     user_inventory()
 
