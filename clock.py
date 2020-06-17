80f947cd5c259d582cf9b18fb2e6c2fcba9267ac
Henrik Håkansson <henrikhakansson95@gmail.com>
2020-06-17 17:09:41 +0200

    Set daily user inventory to UTC 00:00

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ from app.user_inventory import user_inventory
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', hour=15, minute=8)
+@sched.scheduled_job('cron', hour=0)
 def run_user_inventory():
     user_inventory()
 
