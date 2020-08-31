e9f8e7a23c890b633fe8aa85abb32d2e84f01dee
Henrik Håkansson <henrikhakansson95@gmail.com>
2020-08-31 10:10:57 +0200

    Add backwards links in forms & update calender events attributes

--- app/calenderfeeds.py
+++ app/calenderfeeds.py
@@ -20,19 +20,22 @@ def add_activities(root, calendar: Calendar):
             attributes = activity.attrib
             cal_event = Event()
 
-            cal_event['summary'] = activity.find('Name').text + ' [KLUBBAKTIVITET]'
+            cal_event['summary'] = '{} [{} anmälda]'.format(activity.find('Name').text, attributes['registrationCount'])
 
             starttime = parser.parse(attributes['startTime'])
             starttime = starttime.astimezone(timezone)
             cal_event['dtstart'] = vDatetime(starttime).to_ical()
 
-            if starttime.time() == time(0,0,0):
+            if starttime.time() == time(0, 0, 0):
                 endtime = starttime + timedelta(days=1)
             else:
-                endtime = starttime + timedelta(hours=2)
+                endtime = starttime + timedelta(hours=3)
             cal_event['dtend'] = vDatetime(endtime).to_ical()
 
-            cal_event['categories'] = 'EVENTOR'
+            cal_event['categories'] = ','.join(['Eventor', 'Klubbaktivitet'])
+
+            cal_event['description'] = 'Denna aktivitet är importerad från Eventor, för info se: {}'.format(
+                attributes['url'])
 
             cal_event['url'] = attributes['url']
 
@@ -75,9 +78,13 @@ def add_events(root, calendar: Calendar):
 
             cal_event['dtend'] = vDatetime(enddatetime).to_ical()
 
-            cal_event['categories'] = 'EVENTOR'
+            classification = config['EventClassification'][str(event.find('EventClassificationId').text)]
+            cal_event['categories'] = ','.join(['Eventor', classification])
 
-            cal_event['url'] = 'https://eventor.orientering.se/Events/Show/' + event.find('EventId').text
+            url = 'https://eventor.orientering.se/Events/Show/' + event.find('EventId').text
+            cal_event['url'] = url
+
+            cal_event['description'] = 'Denna aktivitet är importerad från Eventor, för info se: {}'.format(url)
 
             cal_event['uid'] = 'Event_' + event.find('EventId').text + '@eventor.orientering.se'
 
