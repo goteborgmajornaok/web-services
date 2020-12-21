9b9cb77a4f301deda8762984d8e838b4952e0c88
Henrik Håkansson <henrikhakansson95@gmail.com>
2020-12-21 17:26:58 +0100

    embarrassing correction

--- definitions.py
+++ definitions.py
@@ -13,6 +13,6 @@ tmp_config.read(TMP_CONFIG, encoding='utf8')
 
 
 def write_tmp():
-    with open('tmp.cfg.', 'w') as f:
+    with open('tmp.cfg', 'w') as f:
         tmp_config.write(f)
     tmp_config.read(TMP_CONFIG, encoding='utf8')
