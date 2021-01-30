2834b63e5cfc23f258d67f4a3353800548b1898d
Henrik Håkansson <henrikhakansson95@gmail.com>
2021-01-30 17:51:24 +0100

    Bug fixes & Split generation & fetching of calendarfeed - Extend end at midnight to next day - Mark cancelled events in name - Fix finish time bug - Cleanup files related to Heroku deployment, ignore .cgi

--- application/calenderfeeds.py
+++ application/calenderfeeds.py
@@ -1,13 +1,15 @@
+import os
 from datetime import date, timedelta, time
-from io import BytesIO
 
 from dateutil import parser
 import pytz
-from flask import Blueprint, make_response
+from flask import Blueprint, make_response, request, jsonify
 from icalendar import Calendar, Event, vDatetime
 
 from application import eventor_utils
-from definitions import config
+from application.request_handler import api_request
+from common import check_api_key
+from definitions import config, ROOT_DIR
 
 calendarfeeds_app = Blueprint('calendarfeeds', __name__)
 
@@ -47,12 +49,20 @@ def add_activities(root, calendar: Calendar):
             continue
 
 
+def is_cancelled(event):
+    if 'EventStatusId' in [t.tag for t in event.iter()]:
+        return event.find('EventStatusId').text == config['Calendar']['cancelled_status_id']
+    return False
+
+
 def add_events(root, calendar: Calendar):
     for event in root:
         try:
             cal_event = Event()
 
             name = event.find('Name').text
+            if is_cancelled(event):
+                name = '[INSTÄLLD] ' + name
 
             org_id = event.find('Organiser').find('OrganisationId').text
             org_name = eventor_utils.org_name(org_id)
@@ -63,8 +73,8 @@ def add_events(root, calendar: Calendar):
             startdatetime = parser.parse(startdate_str + ' ' + starttime_str)
             startdatetime = timezone.localize(startdatetime)
 
-            enddate_str = event.find('StartDate').find('Date').text
-            endtime_str = event.find('StartDate').find('Clock').text
+            enddate_str = event.find('FinishDate').find('Date').text
+            endtime_str = event.find('FinishDate').find('Clock').text
             enddatetime = parser.parse(enddate_str + ' ' + endtime_str)
             enddatetime = timezone.localize(enddatetime)
 
@@ -73,6 +83,9 @@ def add_events(root, calendar: Calendar):
                     enddatetime = startdatetime + timedelta(days=1)
                 else:
                     enddatetime = startdatetime + timedelta(hours=3)
+
+            elif startdatetime.date() != enddatetime.date() and enddatetime.time() == time(0, 0, 0, tzinfo=timezone):
+                enddatetime += timedelta(days=1)
 
             cal_event['dtstart'] = vDatetime(startdatetime).to_ical()
 
@@ -94,8 +107,7 @@ def add_events(root, calendar: Calendar):
             continue
 
 
-@calendarfeeds_app.route('/calendarfeed/<int:days_in_advance>', methods=['GET'])
-def calendarfeed(days_in_advance: int):
+def generate_calendarfeed(days_in_advance: int):
     calendar = Calendar()
     calendar['method'] = 'REQUEST'
     calendar['prodid'] = '-//Svenska Orienteringsförbundet//GMOK'
@@ -103,19 +115,67 @@ def calendarfeed(days_in_advance: int):
 
     start = date.today()
     end = start + timedelta(days=days_in_advance)
+
+    # Fetch club activities
     activities_root = eventor_utils.club_activities(start, end)
     add_activities(activities_root, calendar)
-    districs_events_root = eventor_utils.events(start, end, [1, 2, 3, 4, 6], [config['EventorApi']['district_id']])
+
+    # Fetch district events
+
+    districs_events_root = eventor_utils.events(start, end, config['Calendar']['district_event_class_ids'].split(','),
+                                                [config['EventorApi']['district_id']])
     add_events(districs_events_root, calendar)
 
-    club_events_root = eventor_utils.events(start, end, [5], [config['EventorApi']['organisation_id']])
+    # Fetch club events
+    club_events_root = eventor_utils.events(start, end, config['Calendar']['club_event_class_ids'].split(','),
+                                            [config['EventorApi']['organisation_id']])
     add_events(club_events_root, calendar)
 
-    io = BytesIO()
-    io.write(calendar.to_ical())
+    f = open(config.get('Calendar', 'filename'), 'wb')
+    f.write(calendar.to_ical())
+    f.close()
+
+    return jsonify({'message': 'Calendarfeed successfully generated for next {} days'.format(days_in_advance)})
+
+
+def overwrite_changed(calendar):
+    target_feed = Calendar.from_ical(api_request('GET', config['Calendar']['target_feed'], '', ''))
+
+    target_dict = dict()
+    for component in target_feed.subcomponents:
+        if 'UID' in component and 'DESCRIPTION' in component:
+            target_dict[component['UID']] = component['DESCRIPTION']
+
+    for component in calendar.subcomponents:
+        if 'UID' in component and component['UID'] in target_dict:
+            component['DESCRIPTION'] = target_dict[component['UID']]
+
+
+def fetch_calendarfeed():
+    if not os.path.exists(config['Calendar']['filename']):
+        return jsonify({"message": "Calendarfeed not generated"}), 503
+
+    latest_ics = ROOT_DIR + '/' + config['Calendar']['filename']
+    with open(latest_ics, 'rb') as f:
+        calendar = Calendar.from_ical(f.read())
+
     try:
-        response = make_response(io.getvalue())
+        response = make_response(calendar.to_ical())
         response.headers["Content-Disposition"] = "attachment; filename=Events.ics"
         return response
     except IOError:
         raise Exception(config['Errors']['io_error'], 'eventor')
+
+
+@calendarfeeds_app.route('/calendarfeed', methods=['GET'])
+@calendarfeeds_app.route('/calendarfeed/<int:days_in_advance>', methods=['POST'])
+def calendarfeed(days_in_advance: int = None):
+    if request.method == 'POST':
+        if not check_api_key(request.headers):
+            return jsonify({"message": "ERROR: Unauthorized"}), 401
+        if isinstance(days_in_advance, int):
+            return generate_calendarfeed(days_in_advance)
+        else:
+            return jsonify('Specify how many days to generate feed for'), 400
+    elif request.method == 'GET':
+        return fetch_calendarfeed()
