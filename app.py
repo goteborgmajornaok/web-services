1deb6a7de77e5d218636c850e5f48e792ce61358
Henrik Håkansson <henrikhakansson95@gmail.com>
2022-09-17 22:48:45 +0200

    Ensure configuration file error can be logged

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 #!/usr/local/bin/python3
 # -*- coding: UTF-8 -*-
-from definitions import ROOT_DIR
-import logging
-
-logging.basicConfig(filename=ROOT_DIR + '/app.log',
-                    filemode='a',
-                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    level=logging.INFO)
-
 try:
+    import os
+    import logging
+
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    logging.basicConfig(filename=ROOT_DIR + '/app.log',
+                        filemode='a',
+                        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.INFO)
+
     from wsgiref.handlers import CGIHandler
     from application.main import flaskapp
 
@@ -18,12 +20,19 @@ except (ModuleNotFoundError, ImportError) as e:
     import subprocess
     import sys
 
-    for l in open(ROOT_DIR + f'/requirements.txt'):
-        subprocess.check_call([sys.executable, "-m", "pip", "install", l.rstrip()])
+    for l in open(ROOT_DIR + '/requirements.txt'):
+        logging.info(f'Installing {l.rstrip()}')
+        try:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", l.rstrip()])
+        except Exception as e:
+            logging.error(e)
 
     print("Content-Type: text/html")
     print()
     print("<h1>Environment has been reset. Try reloading page.</h1>")
 except Exception as e:
     logging.error(e)
-    raise e
+    print("Content-Type: text/html")
+    print()
+    print("<h1>Unhandled error</h1>")
+    print("<p>Check log files.</p>")
